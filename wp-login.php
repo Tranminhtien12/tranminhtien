48833e7b6fdd7502bc20d0da4aae568bf8807999
Tranminhtien12 <tranminhtien29122002@gmail.com>
2021-07-08 09:17:00 +0700

    gg

--- wp-login.php
+++ wp-login.php
@@ -89,7 +89,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="./wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"1e84c1d980"};
+var userProfileL10n = {"user_id":"0","nonce":"ea348a22c3"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
